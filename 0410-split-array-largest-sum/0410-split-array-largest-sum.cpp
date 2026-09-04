eab2e7858ea08830c3ac0d9f0e8e93459da9479c
Sameer Raj <143779985+rajsameer6534@users.noreply.github.com>
2026-09-04 19:06:18 +0530

    Time: 0 ms (100%), Space: 10.4 MB (97.48%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    int f(vector<int>& nums, int pages){
-        int stud=1;
-        long long pagesstud=0;
+    int f(vector<int>& nums, int val, int k){
+        int cnt=1;
+        int sum=0;
         for(int i=0;i<nums.size();i++){
-            if(pagesstud+nums[i]<=pages){
-                pagesstud+=nums[i];
-            }
+            if(nums[i]+sum<=val) sum+=nums[i];
             else{
-                stud+=1;
-                pagesstud=nums[i];
+                cnt++;
+                sum=nums[i];
             }
         }
-        return stud;
-    }
-    int findpages(vector<int>& nums, int n ,int k){
-        int lo=*max_element(nums.begin(),nums.end());
-        int hi=accumulate(nums.begin(),nums.end(),0);
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            int students=f(nums,mid);
-            if(students>k){
-                lo=mid+1;
-            }
-            else hi=mid-1;
-        }
-        return lo;
+       return cnt;
+
     }
     int splitArray(vector<int>& nums, int k) {
-        return findpages(nums,nums.size(),k);
+       int lo=*max_element(nums.begin(),nums.end());
+       int hi=accumulate(nums.begin(),nums.end(),0);
+       while(lo<=hi){
+        int mid=lo+(hi-lo)/2;
+        int value =f(nums,mid,k);
+        if(value>k){
+           lo=mid+1;
+        }
+        else hi=mid-1;
+       } 
+       return lo;
     }
 };
